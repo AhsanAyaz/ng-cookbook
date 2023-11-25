a8209f60cde9936d0d30925643572e1ae46c5146
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-11-25 23:22:54 +0100

    fix(seo): remove the home layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [{
-  path: '',
-  pathMatch: 'full',
-  redirectTo: 'home'
-}, {
-  path: 'home',
-  loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
-}, {
-  path: 's/:id',
-  loadComponent: () => import('./snippets/snippets.component').then(m => m.SnippetsComponent)
-}, {
-  path: '**',
-  loadComponent: () => import('./not-found/not-found.component').then(m => m.NotFoundComponent)
-}];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 's/:id',
+    loadComponent: () =>
+      import('./snippets/snippets.component').then((m) => m.SnippetsComponent),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./not-found/not-found.component').then(
+        (m) => m.NotFoundComponent
+      ),
+  },
+];
