f9c414cddb1f5ceeb68d6e3a6fbec180dfa5b142
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2024-01-03 09:49:28 +0100

    feat(analytics): add analytics

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,44 @@
-import { ApplicationConfig, isDevMode } from '@angular/core';
+import {
+  ApplicationConfig,
+  isDevMode,
+  importProvidersFrom,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideServiceWorker } from '@angular/service-worker';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import {
+  getAnalytics,
+  provideAnalytics,
+  ScreenTrackingService,
+} from '@angular/fire/analytics';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withFetch()), provideClientHydration(), provideServiceWorker('ngsw-worker.js', {
-        enabled: !isDevMode(),
-        registrationStrategy: 'registerWhenStable:30000'
-    })]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    provideClientHydration(),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp({
+          projectId: 'ng-cookbook-4d102',
+          appId: '1:1038411915746:web:202192e0e2a689e86419bb',
+          storageBucket: 'ng-cookbook-4d102.appspot.com',
+          apiKey: 'AIzaSyDkwAMJ4ZBGY0AWQsvWy1p8wtCmLjfeu4I',
+          authDomain: 'ng-cookbook-4d102.firebaseapp.com',
+          messagingSenderId: '1038411915746',
+          measurementId: 'G-Z1P15PWEJQ',
+        })
+      )
+    ),
+    importProvidersFrom(provideAnalytics(() => getAnalytics())),
+    ScreenTrackingService,
+  ],
 };
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MailchimpNg2eComponent } from '../components/mailchimp-ng2e/mailchimp-ng2e.component';
+import { getAnalytics, logEvent } from '@angular/fire/analytics';
+
+enum AnalyticsEvent {
+  NGCB1_CLICK = 'ng-cookbook-1 link clicked',
+  NGCB2_CLICK = 'ng-cookbook-2 link clicked',
+}
 
 @Component({
   selector: 'app-home',
@@ -9,4 +15,9 @@ import { MailchimpNg2eComponent } from '../components/mailchimp-ng2e/mailchimp-n
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  events = AnalyticsEvent;
+  sendAnalyticsEvent(event: AnalyticsEvent) {
+    logEvent(getAnalytics(), event);
+  }
+}
