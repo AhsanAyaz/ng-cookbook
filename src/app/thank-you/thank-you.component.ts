ba298f64fe7de9021f6cb21212f68e44a2a5d829
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2024-03-23 02:31:50 +0100

    feat(thank-you): add reviews from pulsechecke

--- src/app/thank-you/thank-you.component.ts
+++ src/app/thank-you/thank-you.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { HeaderComponent } from '../components/header/header.component';
+import { DomSanitizer } from '@angular/platform-browser';
+import { PULSE_CHECKER_SESSIONS } from '../assets/pulsechecker-sessions';
 
 @Component({
   selector: 'app-thank-you',
@@ -8,4 +10,24 @@ import { HeaderComponent } from '../components/header/header.component';
   templateUrl: './thank-you.component.html',
   styleUrl: './thank-you.component.scss',
 })
-export class ThankYouComponent {}
+export class ThankYouComponent {
+  recipeName = input<string>();
+  sanitizer = inject(DomSanitizer);
+  session = computed(() => {
+    const session = PULSE_CHECKER_SESSIONS[this.recipeName() || ''];
+    if (!session) {
+      return null;
+    }
+    return session;
+  });
+  reviewLink = computed(() => {
+    if (!this.session()) {
+      return '';
+    }
+    return this.sanitizer.bypassSecurityTrustResourceUrl(
+      `https://pulsechecker.netlify.app/review-widget?sessionPin=${
+        this.session()!.pin
+      }`
+    );
+  });
+}
