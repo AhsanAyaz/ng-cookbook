e6e76e8c4c393633a7d1f0811a6b7320f0cb2aae
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2024-06-17 00:07:33 +0200

    feat(buy): add buy button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts,scss}",
-  ],
+  content: ["./src/**/*.{html,ts,scss}"],
   theme: {
-    extend: {},
+    extend: {
+      listStyleType: {
+        auto: "auto",
+      },
+    },
   },
   plugins: [],
-}
-
+};
